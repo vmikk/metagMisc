287f74a99585dfc43c3095cfa3fd13348922ff09
vmikk <vmikryukov@gmail.com>
2017-03-13 10:31:38 +0500

    add dfRowName

---
+++ R/dfRowName.R
@@ -0,0 +1,19 @@
+
+#' @title Transfer rownames of data frame to a new column (for data export)
+#'
+#' @param x Data frame
+#' @param name Name of the new column to be created
+#'
+#' @return Data frame
+#' @export
+#'
+#' @examples
+#' x <- as.data.frame(matrix(sample(1:100, 100), nrow = 10, dimnames = list(paste("row", 1:10, sep=""), paste("col", 1:10, sep=""))))
+#' x
+#' dfRowName(x, name = "ColumnWithRowNames")
+dfRowName <- function(x, name = "Rows"){
+	res <- data.frame(rownames(x), x)
+	colnames(res)[1] <- name
+	rownames(res) <- NULL
+	return(res)
+}
