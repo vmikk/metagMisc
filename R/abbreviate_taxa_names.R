84b1d422e40bd4a2987992431a72589b507b209a
vmikk <vmikryukov@gmail.com>
2017-03-17 13:58:59 +0500

    roxigenize abbreviate_taxa_names

--- R/abbreviate_taxa_names.R
+++ R/abbreviate_taxa_names.R
@@ -1,13 +1,26 @@
 
-
-## shorten species name
-# based on vegan::make.cepnames (1b816c1; Aug 2, 2011)
+#' @title Abbreviate taxa names
+#' @param names Character vector of species names
+#' @param nlet Number of letters to take from genus-species parts
+#' @param totl Number of letters in the final abbreviation
+#' @param sep Which separator to use for genus-species case (default is underscore)
+#' @param seconditem Logical, take the second item of the original name for abbreviation
+#'
+#' @description This function is based on vegan::\code{\link{make.cepnames}} (git 1b816c1; Aug 2, 2011).
+#' @return Character vector of abbreviated taxa names
+#' @export
+#' @author Author of the original function is Jari Oksanen (\code{\link{make.cepnames}})
+#' @seealso \code{\link{make.cepnames}}
+#' @examples
+#' x <- c("Laccaria laccata", "Meliniomyces bicolor",
+#'   "Inocybe cincinnata", "Inocybe", "Tylospora asterophora",
+#'   "Cadophora finlandica", "Saccharomycetales")
+#'
+#' abbreviate_taxa_names(x, nlet = 3, totl = 7, sep = "_")
+#' abbreviate_taxa_names(x, nlet = 4, totl = 8, sep = "")   # same as vegan::make.cepnames
+#' vegan::make.cepnames(x)
+#'
 abbreviate_taxa_names <- function(names, nlet = 3, totl = 7, sep="_", seconditem = F){
-  # names = character vector of species names
-  # nlet = number of letters to take from genus-species parts
-  # totl = number of letters in the final abbreviation
-  # sep = which separator to use for genus-species case
-  # seconditem =  take the second item of the original name for abbreviation
 
   ## make valid names
   names <- make.names(names, unique = FALSE)
@@ -40,11 +53,3 @@ abbreviate_taxa_names <- function(names, nlet = 3, totl = 7, sep="_", seconditem
   names <- make.names(names, unique = TRUE)
   return(names)
 }
-
-
-x <- c("Laccaria laccata", "Meliniomyces bicolor",
-       "Inocybe cincinnata", "Inocybe", "Tylospora asterophora",
-       "Cadophora finlandica", "Saccharomycetales")
-abbreviate_taxa_names(x, nlet = 3, totl = 7, sep = "_")
-abbreviate_taxa_names(x, nlet = 4, totl = 8, sep = "")   # same as vegan::make.cepnames
-vegan::make.cepnames(x)
