74ac35c2a4f5a4184c2e3228d263fdeef6e4705d
Vladimir Mikryukov <vmikk@users.noreply.github.com>
2017-03-24 12:01:53 +0500

    accept multiple samples in extract_shared_otus
    Now this function accepts multiple samples. By default it will extract OTUs present in all samples.

--- R/phyloseq_extract_shared_otus.R
+++ R/phyloseq_extract_shared_otus.R
@@ -1,25 +1,27 @@
 
-## Extract species in common between two samples
-phyloseq_extract_shared_otus <- function(x, samp1, samp2){
-	# x = phyloseq object
-	# samp1 and samp2 = sample names (character string)
+## Extract common species (OTUs) between samples
+# by default all samples will be taken
+phyloseq_extract_shared_otus <- function(x, samp_names = sample_names(x)){
+  # x = phyloseq object
+  # samp_names = character vector with sample names
 
-	require(phyloseq)
+  require(phyloseq)
 
-	# test if the sample names are valid
-	if( any(!c(samp1, samp2) %in% sample_names(x)) ){
-		stop("Check the sample names, not all of them are present in the phyloseq object.\n")
-	}
+  # test if the sample names are valid
+  if( any(!samp_names %in% sample_names(x)) ){
+    stop("Check the sample names, not all of them are present in the phyloseq object.\n")
+  }
 
-	# extract samples
-	xx <- prune_samples(samples = c(samp1, samp2), x = x)
+  # extract samples
+  xx <- prune_samples(samples = samp_names, x = x)
 
-	# subset to OTUs that are present in both samples
-	xx <- filter_taxa(xx, function(z){ sum(z >= 1) == 2 }, TRUE)
+  # subset to OTUs that are present in both samples
+  xx <- filter_taxa(xx, function(z){ sum(z >= 1) == length(samp_names) }, TRUE)
 
-	return(xx)
+  return(xx)
 }
 
-
-## example
-phyloseq_extract_shared_otus(esophagus, samp1 = "B", samp2 = "C")
+## Example:
+data(esophagus)
+phyloseq_extract_shared_otus(esophagus, samp_names = sample_names(esophagus))
+phyloseq_extract_shared_otus(esophagus, samp_names = c("B", "C"))
