e636202c8a732227b2df1e0200a6fcc2fa5d44c8
vmikk <vmikryukov@gmail.com>
2017-03-27 13:19:36 +0500

    roxigenize phyloseq_extract_shared_otus

--- R/phyloseq_extract_shared_otus.R
+++ R/phyloseq_extract_shared_otus.R
@@ -1,9 +1,19 @@
 
-## Extract common species (OTUs) between samples
-# by default all samples will be taken
+#' @title Extract common species (OTUs) between samples.
+#' @description This function will subset phyloseq object to the OTUs that are present in all samples. By default all samples will be taken into account, otherwise it's possible to take a subset of samples.
+#'
+#' @param x Phyloseq object
+#' @param samp_names Character vector with sample names (default, all samples)
+#'
+#' @return Phyloseq object with the subset of data.
+#' @export
+#'
+#' @examples
+#' data(esophagus)
+#' phyloseq_extract_shared_otus(esophagus, samp_names = sample_names(esophagus))
+#' phyloseq_extract_shared_otus(esophagus, samp_names = c("B", "C"))
+#'
 phyloseq_extract_shared_otus <- function(x, samp_names = sample_names(x)){
-  # x = phyloseq object
-  # samp_names = character vector with sample names
 
   require(phyloseq)
 
@@ -20,8 +30,3 @@ phyloseq_extract_shared_otus <- function(x, samp_names = sample_names(x)){
 
   return(xx)
 }
-
-## Example:
-data(esophagus)
-phyloseq_extract_shared_otus(esophagus, samp_names = sample_names(esophagus))
-phyloseq_extract_shared_otus(esophagus, samp_names = c("B", "C"))
