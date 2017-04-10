38503a02e1fd15c79aec5f30a0010e58882d4aa4
vmikk <vmikryukov@gmail.com>
2017-04-10 15:08:22 +0500

    phyloseq_sep_samp - add warning for zero-samples

--- R/phyloseq_sep_samp.R
+++ R/phyloseq_sep_samp.R
@@ -10,6 +10,20 @@ phyloseq_sep_samp <- function(physeq, drop_zeroes = T){
         cat("Warning: there is only one sample in the resulting list.\n")
     }
 
+    ## Check if there are samples with zero-totals
+    ssum <- sample_sums(physeq)
+    if(any(ssum == 0)){
+      if(drop_zeroes == FALSE){
+        stop("Error: there are samples with zero total abundance. Use 'drop_zeroes = T' to remove them.\n")
+      }
+      ## Remove samples with missing data
+      if(drop_zeroes == TRUE){
+        snames <- paste(names(ssum)[which(ssum == 0)], collapse = ", ")
+        cat("Warning: samples with zero total abundance were removed.\n(Removed samples: ", snames, ")\n", sep = "")
+        physeq <- prune_samples(ssum > 0, physeq)
+      }
+    }
+
     ## Extract sample names
     smp <- data.frame(samples = sample_names(physeq), stringsAsFactors = F)
 
