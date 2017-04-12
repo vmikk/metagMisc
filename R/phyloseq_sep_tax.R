1420d66b0b5414989c91bc4749babef500ad53ae
vmikk <vmikryukov@gmail.com>
2017-04-12 17:52:12 +0500

    add phyloseq_sep_tax

---
+++ R/phyloseq_sep_tax.R
@@ -0,0 +1,63 @@
+
+
+## Split phyloseq object per groups within selected taxonomic rank
+# This function splits a phyloseq object by specified taxonomic rank, returning a list of objects whose components each correspond to a taxonomic rank.
+phyloseq_sep_tax <- function(physeq, TaxRank = "Phylum", drop_NA = FALSE){
+  ## TO DO - change from for-loop to plyr
+  ##       - verify that the taxonomic names are unique across higher ranks (e.g. no same class-names within different phylums)
+
+  require(phyloseq)
+  require(plyr)
+
+  ## Test if the taxonomic table is present
+  if( is.null(tax_table(physeq)) ){
+    stop("Nothing subset. No taxonomyTable in physeq.\n")
+  }
+
+  ## Test if the specified TaxRank is valid
+  if( !TaxRank %in% rank_names(physeq) ){
+  	stop("Specified taxonomic rank is missing in the taxonomy table of physeq.\n")
+  }
+
+  ## Extract tax table
+  txtbl <- as.data.frame(tax_table(physeq), stringsAsFactors = F)
+
+  ## Remove missing values if required
+  if(drop_NA == TRUE){
+    nas <- is.na(txtbl[, TaxRank])
+    if(any(nas)){ txtbl <- txtbl[-which(nas), ] }
+  }
+
+  ## Repace NAs with character "NA"
+  if(drop_NA == FALSE){
+    nas <- is.na(txtbl[, TaxRank])
+    if(any(nas)){ txtbl[which(nas), TaxRank] <- "NAs" }
+  }
+
+  # Get unique levels within TaxRank
+  rnk <- as.vector( unique(txtbl[, TaxRank]) )
+
+  ## Remove missing values if required
+  # if(drop_NA == TRUE){ rnk <- na.omit(rnk) }
+
+  ## TO DO - subset by each tax rank
+  # dlply(.data=txtbl, .variables=TaxRank, .fun=function(x, ps){
+  #   tax_subs <- as(x, "matrix")
+  #   tax_table(ps) <- tax_table(tax_subs)
+  #   return(ps)
+  #   }, ps = physeq)
+
+  ## Subset by each tax rank
+  res <- list()
+  for(i in 1:length(rnk)){
+    tax_subs <- txtbl[which(txtbl[,TaxRank] == rnk[i]), ]
+    tax_subs <- as(tax_subs, "matrix")
+    ps <- physeq
+    tax_table(ps) <- tax_table(tax_subs)
+    res[[i]] <- ps
+    rm(ps, tax_subs)
+  }
+  names(res) <- rnk
+
+  return(res)
+}
