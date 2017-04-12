ba02bfc7f278d70dd9ddfb38e99a2e503cbdc56f
vmikk <vmikryukov@gmail.com>
2017-04-12 18:52:42 +0500

    parse_funguild  - add taxon levels substitution

--- R/parse_funguild.R
+++ R/parse_funguild.R
@@ -2,6 +2,7 @@
 #' @title Parse FUNGuild database
 #' @description FUNGuild = Fungi + fUNctional + Guild
 #' @param url Funguild databse link
+#' @param tax_name Logical, change numeral taxonomic coding with the corresponding taxonomic levels
 #'
 #' @return Data frame containing the entire FUNGuild database.
 #' @export
@@ -11,7 +12,7 @@
 #' @examples
 #' fg <- parse_funguild()
 #'
-parse_funguild <- function(url = 'http://www.stbates.org/funguild_db.php'){
+parse_funguild <- function(url = 'http://www.stbates.org/funguild_db.php', tax_name = TRUE){
 
   require(XML)
   require(jsonlite)
@@ -26,6 +27,27 @@ parse_funguild <- function(url = 'http://www.stbates.org/funguild_db.php'){
 
   ## Remove IDs
   db$`_id` <- NULL
+
+  if(tax_name == TRUE){
+
+    ## Code legend
+    ## Taxon Level: A numeral corresponding the correct taxonomic level for the taxon
+    taxons <- c(
+      "keyword",                                                       # 0
+      "Phylum", "Subphylum", "Class", "Subclass", "Order", "Suborder", # 3:8
+      "Family", "Subfamily", "Tribe", "Subtribe", "Genus",             # 9:13
+      "Subgenus", "Section", "Subsection", "Series", "Subseries",      # 15:19
+      "Species", "Subspecies", "Variety", "Subvariety", "Form",        # 20:24
+      "Subform", "Form Species")
+
+    ## Table with coding
+    taxmatch <- data.frame(
+      TaxID = c(0, 3:13, 15:26),
+      Taxon = factor(taxons, levels = taxons))
+
+    ## Match taxon codes
+    db$taxonomicLevel <- taxmatch[match(x = db$taxonomicLevel, table = taxmatch$TaxID), "Taxon"]
+  }
 
   # remove rows with missing data
   # which(
