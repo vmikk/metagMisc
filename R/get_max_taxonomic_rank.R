ca97a29f13d1bf781b8d85134c985e775f5263f1
vmikk <vmikryukov@gmail.com>
2017-04-19 13:26:46 +0500

    get_max_taxonomic_rank - add phyloseq as input, fix error with factor levels, order tax ranks

--- R/get_max_taxonomic_rank.R
+++ R/get_max_taxonomic_rank.R
@@ -6,6 +6,21 @@ get_max_taxonomic_rank <- function(x, return_rank_only = FALSE){
 
   require(plyr)
 
+  ## Input data
+  inp_class <- class(x)
+
+  ## If input is of class 'phyloseq'
+  if("phyloseq" %in% inp_class || "taxonomyTable" %in% inp_class){
+    otu_names <- taxa_names(x)
+    x <- as.data.frame(tax_table(x), stringsAsFactors = F)
+  }
+
+  ## If there are factors in the input data frame this can cause an "Error: cannot allocate vector of size ... Gb"
+  ## Convert all factors to character
+  if("factor" %in% laply(.data = x, .fun = class)){
+    x[] <- lapply(x, as.character)
+  }
+
   ## Find the indices of the last non-NA column for each row
   res <- adply(.data = x, .margins = 1, .fun = function(z){
 
@@ -30,6 +45,14 @@ get_max_taxonomic_rank <- function(x, return_rank_only = FALSE){
 
   ## Substitute column index with tax rank name
   res$RankName <- rnks$RankName[match(x = res$RankColumn, table = rnks$RankColumn)]
+
+  ## Reorder taxonomic ranks
+  res$RankName <- factor(res$RankName, levels = colnames(x))
+
+  ## Add OTU name
+  if("phyloseq" %in% inp_class || "taxonomyTable" %in% inp_class){
+    res <- data.frame(TaxaName = otu_names, res, stringsAsFactors = F)
+  }
 
   if(return_rank_only == TRUE){
     res <- res$RankName
