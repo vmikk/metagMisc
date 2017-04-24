99ac0d8302ec53196f69c8daebf922045c5e4e93
vmikk <vmikryukov@gmail.com>
2017-04-24 10:55:19 +0500

    add phyloseq_compare

---
+++ R/phyloseq_compare.R
@@ -0,0 +1,29 @@
+
+## Compare two physeq objects
+phyloseq_compare <- function(phy1, phy2, cols = c("Before", "After")){
+  # phy1 & phy2 = phyloseq objects
+  # cols = column names for phy1 & phy2 in the resulting table
+
+  # number of reads per OTU
+  t1 <- taxa_sums(phy1)
+  t2 <- taxa_sums(phy2)
+
+  # percentage of reads
+  if(sum(t1) > sum(t2)){ pr1 <- 100; pr2 <- (sum(t2)*100)/(sum(t1)) }
+  if(sum(t1) < sum(t2)){ pr2 <- 100; pr1 <- (sum(t1)*100)/(sum(t2)) }
+
+  # percentage of OTUs
+  if(ntaxa(phy1) > ntaxa(phy2)){ po1 <- 100; po2 <- (ntaxa(phy2)*100)/(ntaxa(phy1)) }
+  if(ntaxa(phy1) < ntaxa(phy2)){ po2 <- 100; po1 <- (ntaxa(phy1)*100)/(ntaxa(phy2)) }
+
+  res <- rbind(
+    data.frame(V0 = "Number of OTUs", V1 = ntaxa(phy1), V2 = ntaxa(phy2)),
+    data.frame(V0 = "Percentage of OTUs", V1 = po1, V2 = po2),
+    data.frame(V0 = "Total number of reads", V1 = sum(t1), V2 = sum(t2)),
+    data.frame(V0 = "Percentage of reads", V1 = pr1, V2 = pr2),
+    data.frame(V0 = "Average number of reads per OTU", V1 = mean(t1), V2 = mean(t2)),
+    stringsAsFactors = F)
+
+  colnames(res) <- c("Parameter", cols)
+  return(res)
+}
