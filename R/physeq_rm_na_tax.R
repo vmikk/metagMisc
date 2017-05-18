50e7b2cd9873e240631e8100ca39bb84f6f93145
vmikk <vmikryukov@gmail.com>
2017-05-18 12:13:33 +0500

    rename physeq_rm_na_tax to phyloseq_rm_na_tax

--- R/physeq_rm_na_tax.R
+++ R/physeq_rm_na_tax.R
@@ -2,7 +2,7 @@
 #' @title Remove unused taxonomy levels from phyloseq-object.
 #' @details This function is particularly useful after \code{\link{tax_glom}}.
 #' @param physeq  A phyloseq-class object.
-#' @details 'physeq_rm_na_tax' will remove columns filled with NA values from the taxonomy table of phyloseq object.
+#' @details 'phyloseq_rm_na_tax' will remove columns filled with NA values from the taxonomy table of phyloseq object.
 #' @return Phyloseq object.
 #' @export
 #' @seealso \code{\link{tax_table}}
@@ -13,10 +13,10 @@
 #' head(tax_table(gp))   # last columns are filled with NAs
 #'
 #' # Remove taxonomic levels filled with NAs
-#' gp2 <- physeq_rm_na_tax(gp)  # 5 taxonomic ranks
+#' gp2 <- phyloseq_rm_na_tax(gp)  # 5 taxonomic ranks
 #' head(tax_table(gp2))
 #'
-physeq_rm_na_tax <- function(physeq){
+phyloseq_rm_na_tax <- function(physeq){
   # rm_all <- function(x) { Filter(function(x)!all(is.na(x)), df) }
   rm_all <- function(df) { df[, !apply(is.na(df), 2, all)] }
   tax_table(physeq) <- rm_all( tax_table(physeq) )
