7343a58ee91382d752f2a6dda2b4dfb64509a1a2
vmikk <vmikryukov@gmail.com>
2017-07-28 14:10:39 +0500

    roxygenize filter_cazy

--- R/filter_cazy.R
+++ R/filter_cazy.R
@@ -1,6 +1,15 @@
 
-
-## Filter CAZY table
+#' @title Filter CAZy table
+#' @description This functions filters CAZy annotations based on the total abundance and occurence of CAZy genes across organisms. In other words, it removes CAZy annotations with low abundance.
+#' @param x Data frame with gene counts (every column represents one CAZy annotation and every row represents one organism)
+#' @param min_abund The minimum CAZy count across all organisms to be retained (default, 15)
+#' @param min_occurence The minimum number of organisms with a given CAZy annotation to be retained (default, 10)
+#'
+#' @return Subsetted data frame
+#' @export
+#'
+#' @examples
+#'
 filter_cazy <- function(x, min_abund = 15, min_occurence = 10){
   abund <- colSums(x)
   occur <- apply(x, MARGIN = 2, FUN = function(z){ sum(z>0) })
