dd471cb73880d3b5e464c5d74b17c9fce60409b9
vmikk <vmikryukov@gmail.com>
2017-10-06 15:42:52 +0500

    phyloseq_compare - bugfix with precentage estimation for the cases with equal number of OTUs or total abundance

--- R/phyloseq_compare.R
+++ R/phyloseq_compare.R
@@ -25,11 +25,14 @@ phyloseq_compare <- function(phy1, phy2, cols = c("Before", "After")){
   # percentage of reads
   if(sum(t1) > sum(t2)){ pr1 <- 100; pr2 <- (sum(t2)*100)/(sum(t1)) }
   if(sum(t1) < sum(t2)){ pr2 <- 100; pr1 <- (sum(t1)*100)/(sum(t2)) }
+  if(sum(t1) == sum(t2)){ pr1 <- 100; pr2 <- 100 }
 
   # percentage of OTUs
   if(ntaxa(phy1) > ntaxa(phy2)){ po1 <- 100; po2 <- (ntaxa(phy2)*100)/(ntaxa(phy1)) }
   if(ntaxa(phy1) < ntaxa(phy2)){ po2 <- 100; po1 <- (ntaxa(phy1)*100)/(ntaxa(phy2)) }
+  if(ntaxa(phy1) == ntaxa(phy2)){ po1 <- 100; po2 <- 100 }
 
+  ## Prepare resulting table
   res <- rbind(
     data.frame(V0 = "Number of OTUs", V1 = ntaxa(phy1), V2 = ntaxa(phy2)),
     data.frame(V0 = "Percentage of OTUs", V1 = po1, V2 = po2),
@@ -39,5 +42,6 @@ phyloseq_compare <- function(phy1, phy2, cols = c("Before", "After")){
     stringsAsFactors = F)
 
   colnames(res) <- c("Parameter", cols)
+
   return(res)
 }
