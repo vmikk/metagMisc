decf7aedc6e9ee4b2f87dbdf95c0a7693c960f96
vmikk <vmikryukov@gmail.com>
2017-10-26 10:40:41 +0500

    phyloseq_mult_raref - add parallel option

--- R/phyloseq_mult_raref.R
+++ R/phyloseq_mult_raref.R
@@ -6,6 +6,7 @@
 #' @param MinSizeTreshold Remove samples with number of reads less then this treshold
 #' @param iter Number of rarefication iterations
 #' @param replace Logical, whether to sample with replacement (TRUE) or without replacement (FALSE, default)
+#' @param multithread Logical or integer; if TRUE, attempts to run the function on multiple cores; integer defines the number of cores to use (if it is set to TRUE, all cores will be used)
 #' @param seeds Integer vector used for the reproducible random subsampling (should be of the same length as the number of iterations)
 #' @param ... Additional arguments will be passed to \code{\link{rarefy_even_depth}}
 #'
@@ -29,7 +30,7 @@
 #' # Do not remove OTUs from the dataset that are no longer observed in any sample (have a count of zero in every sample)
 #' phyloseq_mult_raref(esophagus, trimOTUs = F, replace = T, MinSizeTreshold = 210, SampSize = 210, iter = 10)
 #'
-phyloseq_mult_raref <- function(x, SampSize = NULL, MinSizeTreshold = NULL, iter = 1000, replace = F, seeds = NULL, ...){
+phyloseq_mult_raref <- function(x, SampSize = NULL, MinSizeTreshold = NULL, iter = 1000, replace = F, multithread = F, seeds = NULL, ...){
 
   require(plyr)
   require(phyloseq)
@@ -50,11 +51,75 @@ phyloseq_mult_raref <- function(x, SampSize = NULL, MinSizeTreshold = NULL, iter
   ## Prepare seed values
   if(is.null(seeds)){ seeds <- 1:iter }
 
+
+  ###############
+  ############### Cluster setup
+  ###############
+
+  ## Progress bar type for single-threaded plyr functions
+  progr <- "text"
+  parall <- FALSE
+
+  ## Check if foreach and doParallel packages are available
+  if(multithread){
+    if(!requireNamespace("foreach", quietly = TRUE)){ stop("foreach package required for parallel plyr operation.\n") }
+    if(!requireNamespace("doParallel", quietly = TRUE)){ stop("doParallel package is required.\n") }
+  }
+
+  ## Check the platform type and specify the number of cores to use
+  if(multithread && .Platform$OS.type == "unix"){
+    ncores <- parallel::detectCores()
+    if(is.numeric(multithread)){ ncores <- multithread }
+    if(is.na(ncores) | is.null(ncores)){ ncores <- 1 }
+  } else {
+    ncores <- 1
+    if(multithread && .Platform$OS.type=="windows"){
+      warning("Multithreading has been DISABLED, as forking is not supported on .Platform$OS.type 'windows'.\n")
+    }
+  }
+
+  ## Setup cluster
+  if(ncores > 1){
+
+    ## plyr arguments for parallel execution
+    progr <- "none"
+    parall <- TRUE
+
+    ## Disable load balancing
+    paropts <- list(preschedule=TRUE)
+
+    ## Start the cluster
+    cl <- parallel::makeCluster(ncores)
+
+    ## Register the parallel backend
+    doParallel::registerDoParallel(cl)
+
+    ## Load packages on cluster nodes
+    parallel::clusterEvalQ(cl, library("phyloseq"))
+
+    ## Send useful objects to the workers
+    vars <- c("SampSize", "replace")
+    parallel::clusterExport(cl=cl, varlist=vars, envir = environment())
+  }
+
+
+  ###############
+  ############### Rarefy
+  ###############
+
   ## Rarefy
   res <- mlply(
     .data = seeds,
-    .fun = function(z){ rarefy_even_depth(x, rngseed=z, sample.size=SampSize, replace=replace, verbose = F, ...) },
-    .progress = "text")
+    .fun = function(z, ...){ phyloseq::rarefy_even_depth(x, rngseed=z, sample.size=SampSize, replace=replace, verbose = F, ...) },
+    .progress = progr,
+    .parallel = parall,
+    ...)  # pass additional arguments to  phyloseq::rarefy_even_depth
+
+  ## Stop the cluster
+  if(ncores > 1){
+    parallel::stopCluster(cl)
+    rm(cl)
+  }
 
   ## Add rarefaction parameters as attributes to the resulting list
   attr(res, which = "RarefactionDepth") <- SampSize
