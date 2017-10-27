a28d6cac3c3e8be55a5f735775bb698be5d047b7
vmikk <vmikryukov@gmail.com>
2017-10-27 09:54:25 +0500

    prevalence - add mean and median OTU abundance, update docs

--- R/prevalence.R
+++ R/prevalence.R
@@ -1,5 +1,5 @@
 
-#' @title Compute prevalence of each species.
+#' @title Compute prevalence and abundance summary of each species/OTU.
 #'
 #' @param physeq A phyloseq-class object
 #' @param add_tax Logical, add taxonomy to the results
@@ -14,13 +14,29 @@
 #' head( prevalence(GlobalPatterns, add_tax = T) )
 #'
 prevalence <- function(physeq, add_tax = TRUE){
-  prevdf <- apply(X = otu_table(physeq),
-                  MARGIN = ifelse(taxa_are_rows(physeq), yes = 1, no = 2),
+
+  ## Check if taxa are rows
+  trows <- taxa_are_rows(physeq)
+
+  ## Extract OTU table
+  otutab <- as.data.frame(otu_table(physeq))
+
+  ## Transpose OTU table (species should be arranged by rows)
+  if(trows == FALSE){
+    otutab <- t(otutab)
+  }
+
+  ## Estimate prevalence (number of samples with OTU present)
+  prevdf <- apply(X = otutab,
+                  # MARGIN = ifelse(trows, yes = 1, no = 2),  # for a non-transposed data
+                  MARGIN = 1,
                   FUN = function(x){sum(x > 0)})
 
-  ## Add taxonomy and total read counts to this data.frame
+  ## Add total and average read counts per OTU
   prevdf <- data.frame(Prevalence = prevdf,
-                       TotalAbundance = taxa_sums(physeq))
+                       TotalAbundance = taxa_sums(physeq),
+                       MeanAbundance = rowMeans(otutab),
+                       MedianAbundance = apply(otutab, 1, median))
 
   ## Add taxonomy table
   if(add_tax == TRUE && !is.null(tax_table(physeq, errorIfNULL = F))){
