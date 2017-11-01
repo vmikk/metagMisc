5fe6232c47e6d1503abf3d97c6da452f01535c74
vmikk <vmikryukov@gmail.com>
2017-11-01 15:21:29 +0500

    roxygenize add_metadata

--- R/add_metadata.R
+++ R/add_metadata.R
@@ -1,4 +1,16 @@
 
+#' @title Add metadata to data.frame
+#' @description This function combines two tables based on the row identifiers.
+#' @param x Data frame with data of interest
+#' @param metad Data frame with metadata that should be added; or phyloseq object with sample_data slot
+#' @param xid Character, name of the column with row identifiers in the main data (x)
+#' @param mid Character, name of the column with row identifiers in the metadata (metad)
+#' @param drop_mid Logical; if TRUE (default), metadata identifiers will be removed from results
+#' @return Data frame
+#' @export
+#'
+#' @examples
+#'
 add_metadata <- function(x, metad, xid, mid, drop_mid = T){
 
   ## Match metadata to the main data
