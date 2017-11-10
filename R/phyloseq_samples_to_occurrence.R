a1881dcceb13a7b803b073de6b84120a22fd240d
vmikk <vmikryukov@gmail.com>
2017-11-10 10:12:04 +0500

    add phyloseq_samples_to_occurrence

---
+++ R/phyloseq_samples_to_occurrence.R
@@ -0,0 +1,90 @@
+
+## Function to estimate species occurrence (within groups of samples)
+phyloseq_samples_to_occurrence <- function(physeq, variable = NULL,
+  taxa_frequency = FALSE, drop_zeroes = FALSE, justdf = FALSE, long = FALSE){
+
+  require(plyr)
+  require(reshape2)
+
+  ## Function to collapse samples into occurrences for a single sample group
+  single_group_occurrence <- function(phys, rel = FALSE){
+
+    ## Transform OTU abundances into presence-absence form
+    sp_count <- apply(X = otu_table(phys),
+        MARGIN = ifelse(taxa_are_rows(phys), yes = 1, no = 2),
+        FUN = function(x){sum(x > 0)})
+
+    ## Absolute occurrence (e.g., number of samples with the species)
+    if(rel == FALSE){
+      rez <- data.frame(Taxa = names(sp_count), Occurrence = sp_count, stringsAsFactors = F)
+    }
+
+    ## Relative occurrence (frequency)
+    if(rel == TRUE){
+      rez <- data.frame(Taxa = names(sp_count), Occurrence = sp_count / nsamples(phys), stringsAsFactors = F)
+    }
+
+    rownames(rez) <- NULL
+    return(rez)
+  }
+
+  ## If no sample groups are specified
+  if(is.null(variable)){
+    res <- single_group_occurrence(physeq, rel = taxa_frequency)
+
+  ## If multiple groups are specified
+  } else {
+
+    ## Split phyloseq by group
+    pg <- phyloseq_sep_variable(physeq, variable, drop_zeroes = FALSE)
+
+    ## Count species occurrence within each group
+    resl <- ldply(.data = pg, .fun = single_group_occurrence, rel = taxa_frequency, .id = "SampleGroup")
+
+    ## Reshape species occurrences into a wide format (samples as columns)
+    res <- dcast(data = resl, Taxa ~ SampleGroup, value.var = "Occurrence")
+
+  }
+
+  ## Remove species with zero abundance (only from the wide-format data)
+  if(drop_zeroes == TRUE){
+    spsums <- rowSums(res[,-1]) > 0
+    if(any(!spsums)){
+      res <- res[spsums, ]
+    }
+  }
+
+  ## Return only species occurrences
+  if(justdf == TRUE){
+
+    ## Return single-group data
+    if(is.null(variable)){ return(res) }
+
+    ## Return multiple-group data
+    if(long == FALSE & !is.null(variable)){
+      return(res)                    # return data in wide format
+    } else {
+      colnames(resl)[1] <- variable  # rename sample group variable in long data
+      return(resl)                   # return data in long format
+    }
+
+  }
+
+  ## Return transformed phyloseq object
+  if(justdf == FALSE){
+
+    ## Remove sample metadata
+    if(!is.null(sample_data(physeq, errorIfNULL = T))){
+      physeq@sam_data <- NULL
+    }
+
+    ## Prepare new OTU table
+    rownames(res) <- res$Taxa
+    res$Taxa <- NULL
+
+    ## Replace OTU table with the new one
+    otu_table(physeq) <- otu_table(res, taxa_are_rows = T)
+
+    return(physeq)
+  }
+}
