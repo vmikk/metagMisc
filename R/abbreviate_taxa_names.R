de232d8fec39b89c083cf116862bc8bcb34aa865
vmikk <vmikryukov@gmail.com>
2017-12-12 18:02:02 +0500

    abbreviate_taxa_names - declare imports from other packages (::)

--- R/abbreviate_taxa_names.R
+++ R/abbreviate_taxa_names.R
@@ -23,7 +23,7 @@
 abbreviate_taxa_names <- function(names, nlet = 3, totl = 7, sep="_", seconditem = F){
 
   ## make valid names
-  names <- make.names(names, unique = FALSE)
+  names <- vegan::make.names(names, unique = FALSE)
 
   ## remove trailing and duplicated dots
   names <- gsub("\\.[\\.]+", ".", names)
@@ -50,6 +50,6 @@ abbreviate_taxa_names <- function(names, nlet = 3, totl = 7, sep="_", seconditem
   names <- abbreviate(names, minlength = totl)
 
   ## Final clean-up
-  names <- make.names(names, unique = TRUE)
+  names <- vegan::make.names(names, unique = TRUE)
   return(names)
 }
