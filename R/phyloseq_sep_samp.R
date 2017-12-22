4450994a24159b01562958aa22f268ca5b6a786e
vmikk <vmikryukov@gmail.com>
2017-12-22 10:31:54 +0500

    phyloseq_sep_samp - declare imports from other packages (::)

--- R/phyloseq_sep_samp.R
+++ R/phyloseq_sep_samp.R
@@ -11,15 +11,16 @@
 #' phyloseq_sep_samp(esophagus)
 #'
 phyloseq_sep_samp <- function(physeq, drop_zeroes = T){
-    require(phyloseq)
-    require(plyr)
+    
+    # require(phyloseq)
+    # require(plyr)
 
-    if(nsamples(physeq) == 1){
+    if(phyloseq::nsamples(physeq) == 1){
         cat("Warning: there is only one sample in the resulting list.\n")
     }
 
     ## Check if there are samples with zero-totals
-    ssum <- sample_sums(physeq)
+    ssum <- phyloseq::sample_sums(physeq)
     if(any(ssum == 0)){
       if(drop_zeroes == FALSE){
         stop("Error: there are samples with zero total abundance. Use 'drop_zeroes = T' to remove them.\n")
@@ -28,20 +29,20 @@ phyloseq_sep_samp <- function(physeq, drop_zeroes = T){
       if(drop_zeroes == TRUE){
         snames <- paste(names(ssum)[which(ssum == 0)], collapse = ", ")
         cat("Warning: samples with zero total abundance were removed.\n(Removed samples: ", snames, ")\n", sep = "")
-        physeq <- prune_samples(ssum > 0, physeq)
+        physeq <- phyloseq::prune_samples(ssum > 0, physeq)
       }
     }
 
     ## Extract sample names
-    smp <- data.frame(samples = sample_names(physeq), stringsAsFactors = F)
+    smp <- data.frame(samples = phyloseq::sample_names(physeq), stringsAsFactors = F)
 
     ## Extract samples
-    res <- mlply(.data = smp, .fun = function(samples){ prune_samples(samples, x = physeq) })
+    res <- plyr::mlply(.data = smp, .fun = function(samples){ phyloseq::prune_samples(samples, x = physeq) })
     names(res) <- smp$samples
 
     ## Remove taxa with zero abundance
     if(drop_zeroes == TRUE){
-        res <- llply(.data = res, .fun = function(x){ prune_taxa(taxa_sums(x) > 0, x) })
+        res <- plyr::llply(.data = res, .fun = function(x){ phyloseq::prune_taxa(phyloseq::taxa_sums(x) > 0, x) })
     }
 
     return(res)
