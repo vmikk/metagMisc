10d2b859128b194bec6d4c566ac5cac1d0eb26c4
vmikk <vmikryukov@gmail.com>
2018-01-16 17:40:23 +0500

    dfRowName - add stringsAsFactors argument

--- R/dfRowName.R
+++ R/dfRowName.R
@@ -11,9 +11,9 @@
 #' x <- as.data.frame(matrix(sample(1:100, 100), nrow = 10, dimnames = list(paste("row", 1:10, sep=""), paste("col", 1:10, sep=""))))
 #' x
 #' dfRowName(x, name = "ColumnWithRowNames")
-dfRowName <- function(x, name = "Rows"){
-	res <- data.frame(rownames(x), x)
-	colnames(res)[1] <- name
-	rownames(res) <- NULL
-	return(res)
+dfRowName <- function(x, name = "Rows", stringsAsFactors = FALSE){
+  res <- data.frame(rownames(x), x, stringsAsFactors = stringsAsFactors)
+  colnames(res)[1] <- name
+  rownames(res) <- NULL
+  return(res)
 }
