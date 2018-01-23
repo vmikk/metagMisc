46f82271fb50cc5d0b6e60358e15b2d57dbd7ebe
vmikk <vmikryukov@gmail.com>
2018-01-23 12:24:19 +0500

    phyloseq_mult_raref_avg - remove slow for-loop for OTU merging; small fixes

--- R/phyloseq_mult_raref_avg.R
+++ R/phyloseq_mult_raref_avg.R
@@ -1,6 +1,6 @@
 
 ## Perform rarefaction and average relative OTU abundance
-phyloseq_mult_raref_avg <- function(z, SampSize = NULL, iter = 1000, parallel = FALSE, ...){
+phyloseq_mult_raref_avg <- function(physeq, SampSize = NULL, iter = 1000, parallel = FALSE, ...){
 
   # require(compositions)
   # require(plyr)
@@ -32,44 +32,82 @@ phyloseq_mult_raref_avg <- function(z, SampSize = NULL, iter = 1000, parallel = 
     sampresent <- FALSE   # no sample_data in pheloseq
   }
 
+
   ## Rarefy (do not remove zero-OTUs)
   cat("..Multiple rarefaction\n")
-  phys_raref <- phyloseq_mult_raref(z, SampSize = SampSize, iter = iter, multithread = parallel, trimOTUs = F, ...)
+  phys_raref <- phyloseq_mult_raref(physeq, SampSize = SampSize, iter = iter, multithread = parallel, trimOTUs = F, ...)
 
   ## Rename rarefied samples (add raref ID)
   cat("..Sample renaming\n")
+  samps_names <- vector()
   for(i in 1:length(phys_raref)){
-    phyloseq::sample_names(phys_raref[[i]]) <- paste(phyloseq::sample_names(phys_raref[[i]]), i, sep="_")
+    tmp_name <- paste(phyloseq::sample_names(phys_raref[[i]]), i, sep="__")
+    samps_names <- c(samps_names, tmp_name)
+    phyloseq::sample_names(phys_raref[[i]]) <- tmp_name
+    rm(tmp_name)
   }
   rm(i)
 
-  ## Combine rarefied OTU tables into a single table
+  ### Combine rarefied OTU tables into a single table
   cat("..Rarefied data merging\n")
-  phys <- phys_raref[[1]]
-  for(i in 2:length(phys_raref)){
-    phys <- phyloseq::merge_phyloseq(phys, phys_raref[[i]])
-  }
-  rm(i)
+  ## using for-loop and merge_phyloseq  -- very slow for large number of iterations
+  # phys <- phys_raref[[1]]
+  # for(i in 2:length(phys_raref)){
+  #   phys <- phyloseq::merge_phyloseq(phys, phys_raref[[i]])
+  # }
+  # rm(i)
+
+  ## Extract OTU tables
+  phys_tabs <- plyr::llply(.data = phys_raref, .fun = function(x){
+    as.data.frame(phyloseq::otu_table(x))
+  })
+
+  ## Merge OTU tables
+  # phys_tabs <- do.call("cbind", phys_tabs)  # loses rownames
+  phys_tabs <- do.call(cbind, phys_tabs)      # why does it alters column names??
+  colnames(phys_tabs) <- samps_names
+
+  ## Create metadata
+  metad <- data.frame(
+    SampleID = gsub(pattern = "__[0-9]+$", replacement = "", x = samps_names),  # remove rarefaction ID
+    # SampleRarefName = samps_names,
+    stringsAsFactors = FALSE
+    )
+  rownames(metad) <- samps_names
+
+  ## Create phyloseq object with merged data
+  phys <- phyloseq::phyloseq(
+            phyloseq::otu_table(phys_tabs, taxa_are_rows = TRUE),
+            phyloseq::sample_data(metad))
+
+  ## Extract rarefaction attributes and remove rarefied data to save RAM
+  ## Add rarefaction parameters as attributes to the phyloseq object
+  RarefactionDepth <- attr(phys_raref, which = "RarefactionDepth")
+  RarefactionReplacement <- attr(phys_raref, which = "RarefactionReplacement")
+  rm(phys_raref, phys_tabs)
 
   ## Split by sample
   cat("..Splitting by sample\n")
-  smps <- phyloseq_sep_variable(phys, variable = "MiSeqSample", drop_zeroes = T)
+  smps <- phyloseq_sep_variable(phys, variable = "SampleID", drop_zeroes = T)
 
   ## Average relative OTU abundances within each sample across rarefaction iterations
   cat("..OTU abundance averaging within rarefaction iterations\n")
-
+  
   ## Function to average OTU table
-  OTU_average <- function(x){
-    # x = phyloseq
+  OTU_average <- function(xx){
+    # xx = phyloseq
+    
+    ## Remove OTUs with zero abundance
+    # xx <- phyloseq::prune_taxa(phyloseq::taxa_sums(xx) > 0, xx)   # zeroes are already removed
 
     ## Convert to relative OTU abundance
-    xx <- phyloseq::transform_sample_counts(x, function(OTU) OTU/sum(OTU) )
+    xx <- phyloseq::transform_sample_counts(xx, function(OTU) OTU/sum(OTU) )
 
     ## Extract OTU table
     otus <- as.data.frame(phyloseq::otu_table(xx))
 
     ## Transpose OTU abundance table (samples must be ROWS from this step!)
-    if(phyloseq::taxa_are_rows(x) == TRUE){
+    if(phyloseq::taxa_are_rows(xx) == TRUE){
       otus <- t(otus)
     }
 
@@ -105,18 +143,17 @@ phyloseq_mult_raref_avg <- function(z, SampSize = NULL, iter = 1000, parallel = 
   ## Re-create phyloseq object
   cat("..Re-create phyloseq object\n")
   res <- phyloseq::phyloseq(
-            phyloseq::otu_table(smps_avg_wide, taxa_are_rows = TRUE),
-            phyloseq::tax_table(z),
-            phyloseq::sample_data(z))
+            phyloseq::otu_table(smps_avg_wide, taxa_are_rows = TRUE)
+            )
 
   ## Recover phyloseq slots
   if(taxpresent == TRUE){ res <- phyloseq::merge_phyloseq(res, taxx) }
   if(phypresent == TRUE){ res <- phyloseq::merge_phyloseq(res, phyy) }
   if(sampresent == TRUE){ res <- phyloseq::merge_phyloseq(res, samm) }
-  
+
   ## Add rarefaction parameters as attributes to the phyloseq object
-  attr(res, which = "RarefactionDepth") <- attr(phys_raref, which = "RarefactionDepth")
-  attr(res, which = "RarefactionReplacement") <- attr(phys_raref, which = "RarefactionReplacement")
+  attr(res, which = "RarefactionDepth") <- RarefactionDepth
+  attr(res, which = "RarefactionReplacement") <- RarefactionReplacement
 
   return(res)
 }
