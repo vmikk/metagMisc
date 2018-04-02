c3ad2a89c72ad3da06c5d51826aa475bbd1a036d
vmikk <vmikryukov@gmail.com>
2018-04-02 12:46:49 +0500

    phyloseq_to_MetaCommunity - add example to docs

--- R/phyloseq_to_MetaCommunity.R
+++ R/phyloseq_to_MetaCommunity.R
@@ -11,7 +11,30 @@
 #' @export
 #'
 #' @examples
-#'
+#' # Load data
+#' data("esophagus")
+#' 
+#' library(entropart)
+#' 
+#' # Convert phyloseq to MetaCommunity class
+#' eso <- phyloseq_to_MetaCommunity(esophagus)
+#' summary(eso)
+#' 
+#' # Estimate diversity (Shannon diversity, q=1)
+#' ad <- AlphaDiversity(eso, q = 1, Correction = "None")
+#' summary(ad)
+#' plot(ad)
+#' 
+#' # Diversity partitioning into alpha and beta components
+#' dp <- DivPart(q = 1, eso, Correction = "None")
+#' summary(dp)
+#' plot(dp)
+#' 
+#' # Diversity profile
+#' dpr <- DivProfile(q.seq = seq(0, 2, 0.1), eso, Correction = "None")
+#' summary(dpr)
+#' plot(dpr)
+#' 
 phyloseq_to_MetaCommunity <- function(physeq, wei = NULL){
 
   # require(entropart)
