0f6e6f5814a5e626faf8d39252c8fb7f488feb1a
vmikk <vmikryukov@gmail.com>
2018-05-07 16:31:06 +0500

    add phyloseq_effort_div_rangeplot

--- R/phyloseq_effort.R
+++ R/phyloseq_effort.R
@@ -27,3 +27,101 @@ phyloseq_effort_div <- function(physeq, base = "size", level = NULL, conf = 0.95
 }
 # phyloseq_effort_div(esophagus, level = 3000)
 # phyloseq_effort_div(esophagus, level = 3000, conf = NULL)
+
+
+
+## Estimate OTU diversity and sample coverage for a particular sequencing depth
+## Estimate required sequencing depth and OTU diversity for a particular sample coverage
+phyloseq_effort_div_rangeplot <- function(physeq, range, range_type = "coverage",
+                                          variable = NULL, yvar = "seqdepth", interval = "quartiles", 
+                                          show_plot = TRUE, justDF = FALSE, progress = "text", ...){
+  # range = vector of levels to test, e.g. seq(from = 100, to = 1000, by = 100)
+  # range_type = "coverage" or "seqdepth"
+  # Q = Hill's q-value (default = 0 - OTU number; 1 = Shannon diversity) - in case of range_type = "seqdepth"
+  # variable = groupping variable
+  # yvar = which dependent variable to show ("coverage", "seqdepth", "Q1", "Q2", "Q3")
+  #    for range_type = "seqdepth": "coverage", "Q0", "Q1", "Q2"
+  #    for range_type = "coverage": "seqdepth", "Q0", "Q1", "Q2"
+  # interval = "CI" or "quartiles" or NULL - which interval to show on the group-averaged data
+  # justDF
+  
+  ## TO DO - add automatic range sizes
+  # range <- seq(from = 100, to = 500, by = 20)      # check seq depth distribution
+  # range <- seq(from = 0.8, to = 0.98, by = 0.02)   # "coverage"
+  
+  ## Estimate required sequencing depth and OTU diversity for a particular sample coverage
+  if(range_type == "coverage"){
+    efforts <- plyr::mdply(
+      .data = data.frame(level = range),
+      .fun = function(...){ phyloseq_effort_div(physeq, base="coverage", conf = NULL, ...) },
+      .progress = progress)
+    
+    ## Replace estimated coverage with the supplied coverage (they are different because of numerical issues)
+    efforts$SC <- efforts$level
+  }
+  
+  ## Estimate OTU diversity and sample coverage for a particular sequencing depth
+  if(range_type == "seqdepth"){
+    efforts <- plyr::mdply(
+      .data = data.frame(level = range),
+      .fun = function(...){ phyloseq_effort_div(physeq, base="size", conf = NULL, ...) },
+      .progress = progress)
+  }
+  
+  ## Return data (otherwise make a plot)
+  if(justDF == TRUE){ return(efforts) }
+  
+  ## Which variables to take for plotting (colnames from iNEXT::estimateD results)
+  if(yvar == "coverage"){ yy <- "SC"; ylab <- "Sample coverage" }
+  if(yvar == "seqdepth"){ yy <- "m"; ylab <- "Sequencing depth (number of reads)" }
+  if(yvar == "Q0"){ yy <- "q = 0"; ylab <- "Species richness, Q = 0" }
+  if(yvar == "Q1"){ yy <- "q = 1"; ylab <- "Species diversity, Q = 1" }
+  if(yvar == "Q2"){ yy <- "q = 2"; ylab <- "Species diversity, Q = 2" }
+  
+  if(range_type == "coverage"){ xx <- "SC"; xlab <- "Sample coverage" }
+  if(range_type == "seqdepth"){ xx <- "m"; xlab <- "Sequencing depth (number of reads)" }
+  
+  if(interval == "CI"){ il <- "CI.lo"; iu <- "CI.up" }
+  if(interval == "quartiles"){ il <- "Qart.lo"; iu <- "Qart.up" }
+  
+  ## Average results for plotting
+  avgeff <- function(z, yy){
+    z <- z[, yy]                                 # extract dependent variable
+    cis <- ggplot2::mean_cl_boot(z, na.rm=TRUE)  # bootstap CI
+    rez <- data.frame(
+      Mean = cis[[1]],
+      CI.lo = cis[[2]],
+      CI.up = cis[[3]],
+      Qart.lo = quantile(z, probs = 0.25),
+      Qart.up = quantile(z, probs = 0.75))
+    rownames(rez) <- NULL
+    return(rez)
+  }
+  # e.g., avgeff(efforts, yy = "q = 1")
+  
+  ## If multiple groups are provided
+  if(!is.null(phyloseq::sample_data(physeq, errorIfNULL = FALSE)) & !is.null(variable)){
+    res_avg <- plyr::ddply(.data=efforts, .variables=c(variable, xx), .fun=avgeff, yy = yy)
+    pp <- ggplot(data = res_avg, aes_string(x = xx, y = "Mean", color = variable, group = variable)) +
+      geom_line() + 
+      geom_ribbon(aes_string(ymin = il, ymax = iu, color = NULL, fill = variable), alpha = 0.2)
+    
+  } else {
+    
+    ## Single group
+    res_avg <- plyr::ddply(.data=efforts, .variables=xx, .fun=avgeff, yy = yy)
+    pp <- ggplot(data = res_avg, aes_string(x = xx, y = "Mean")) +
+      geom_line() + 
+      geom_ribbon(aes_string(ymin = il, ymax = iu), alpha = 0.2)
+  }
+  
+  ## Add axes labels
+  pp <- pp + labs(x = xlab, y = ylab)
+  
+  ## Add raw data to the results
+  attr(pp, which = "Data") <- efforts
+  attr(pp, which = "DataAverage") <- res_avg
+  
+  if(show_plot == TRUE){ print(pp) }
+  invisible(pp)
+}
