d040c1beb1082ff531874712f150911d9ce7017e
vmikk <vmikryukov@gmail.com>
2018-07-03 09:38:48 +0500

    some - now accepts phyloseq as input

--- R/some.R
+++ R/some.R
@@ -1,17 +1,51 @@
-#' @title Check random lines in the data
+#' @title Check random lines in the data (including phyloseq objects)
 #'
-#' @description Returns several random lines from the data.frame or matrix.
+#' @description Returns several random lines from the data.frame, matrix or phyloseq.
 #' Usage is quite similar to the \code{head} or \code{tail} functions.
 #'
-#' @param x A data.frame or matrix
-#' @param n Number of records to show
-#' @return Part of the data.frame or matrix with n lines.
+#' @param x Coluld be a "data.frame", "matrix", or "phyloseq" class
+#' @param n Number of records (or samples in phyloseq) that will be returned
+#' @param n_otu Number of OTUs to show (if the input is phyloseq-object)
+#' @return Part of the data.frame or matrix with n lines or a subset of phyloseq-object.
 #' @author Adapted from \code{some} in the car-package by John Fox.
 #' @examples
 #' x <- matrix(rnorm(500), ncol=5)
 #' some(x)
 #'
-some <- function(x, n=10){
-  show <- sample(x = 1:nrow(x), size = 10)
-  return( x[show, ] )
+some <- function(x, n = 10, n_otu = 10){
+
+  ## Input data = data.frame or matrix
+  if(any(class(x) %in% c("data.frame", "matrix"))){
+    show <- sample(x = 1:nrow(x), size = n)
+    res <- x[show, ]
+  }
+
+  ## Input data = phyloseq object
+  if("phyloseq" %in% class(x)){
+
+    ## How many samples?    
+    nsamps <- phyloseq::nsamples(x)
+
+    ## Subset samples
+    if(nsamps > n){
+      samps <- sample(x = 1:nsamps, size = n)
+      res <- phyloseq::prune_samples(1:nsamps %in% samps, x)
+    } else {
+      warning("Number of samples in phyloseq <= n \n")
+    }
+ 
+    ## How many taxa?
+    ntax <- phyloseq::ntaxa(res)
+
+    ## Subset OTUs
+    if(!is.null(n_otu) & ntax > n_otu){
+      otus <- sample(x = 1:ntax, size = n_otu)
+      res <- phyloseq::prune_taxa(1:ntax %in% otus, res)
+    } else {
+      warning("Number of OTUs in phyloseq <= n_otu \n")
+    }
+
+  } # end of phyloseq
+
+  return(res)
 }
