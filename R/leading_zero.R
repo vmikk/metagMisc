c149c8042673abdd403bd4452566bc0996de425b
Vladimir Mikryukov <vmikryukov@gmail.com>
2018-10-02 09:14:34 +0500

    leading_zero - add auto detection of the number of digits

--- R/leading_zero.R
+++ R/leading_zero.R
@@ -1,9 +1,15 @@
 
-## Add leading zeroes to numeric vector
-leading_zero <- function(x, z=3){
-    # x = numeric vector
-    # z = resulting length of number
-    zz <- paste("%0", z, "d", sep="")
-    sprintf(zz, x)
+## Function to add leading zeroes to numeric vector
+leading_zero <- function(x, z = NULL){
+  # x = numeric vector
+  # z = resulting length of number
+  if(is.null(z)){
+    ## Find the number of characters
+    z <- nchar(as.integer(max(x)))
+  }
+
+  zz <- paste("%0", z, "d", sep = "")
+  sprintf(zz, x)
 }
 # leading_zero(1:10, z=3)
+# leading_zero(c(1,10,100,1000,10000))
