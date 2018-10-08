a9fe4a347588c5622cb0c8b0339b2d0dd940448b
Vladimir Mikryukov <vmikryukov@gmail.com>
2018-10-08 15:51:57 +0500

    leading_zero - combine into a sigle function

--- R/leading_zero.R
+++ R/leading_zero.R
@@ -1,22 +1,28 @@
 
-## Function to add leading zeroes to numeric vector
-leading_zero <- function(x, z = NULL){
+## Function to add or remove leading zeros to numeric vector
+leading_zero <- function(x, z = NULL, mode = "add"){
   # x = numeric vector
   # z = resulting length of number
-  if(is.null(z)){
-    ## Find the number of characters
-    z <- nchar(as.integer(max(x)))
+  # mode = "add" or "remove" leading zero
+
+  ## Add leading zeros
+  if(mode == "add"){
+    if(is.null(z)){
+      ## Find the number of characters
+      z <- nchar(as.integer(max(x)))
+    }
+  
+    zz <- paste("%0", z, "d", sep = "")
+    res <- sprintf(zz, x)
   }
 
-  zz <- paste("%0", z, "d", sep = "")
-  sprintf(zz, x)
+  ## Remove leading zeros
+  if(mode == "remove"){
+    res <- gsub("(^|[^0-9])0+", "\\1", x, perl = TRUE)
+  }
+
+  return(res)
 }
 # leading_zero(1:10, z=3)
 # leading_zero(c(1,10,100,1000,10000))
-
-
-## Remove leading zeroes
-leading_zero_remove <- function(x){
-    gsub("(^|[^0-9])0+", "\\1", x, perl = TRUE)
-}
-leading_zero_remove(c("001", "1001", "g01", "0abc"))
+# leading_zero(c("001", "1001", "g01", "0abc"), mode = "remove")
