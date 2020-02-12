3719814f7de16513236a1beaaee6000de750a57a
Vladimir Mikryukov <vmikryukov@gmail.com>
2020-02-12 11:51:39 +0500

    phyloseq_combine_samples - add comments

--- R/phyloseq_combine_samples.R
+++ R/phyloseq_combine_samples.R
@@ -1,9 +1,15 @@
 ## Function to combine samples (sum OTU abundance) into a single sample
 phyloseq_combine_samples <- function(phys){
 
+  ## Remove sample metadata
   if(!is.null(sample_data(phys, errorIfNULL=F))){ phys@sam_data <- NULL }
+  
+  ## Estimate OTU total abundance 
   tss <- taxa_sums(phys)
+
+  ## Replace OTU table
   ott <- matrix(data = tss, ncol = 1, dimnames = list(names(tss), "Total"))
   otu_table(phys) <- otu_table(ott, taxa_are_rows = TRUE)
+  
   return(phys)
 }
