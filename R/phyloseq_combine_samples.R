f83266e9abc622b15f4f92f8bbeaae6ccd18637c
Vladimir Mikryukov <vmikryukov@gmail.com>
2020-02-13 13:37:57 +0500

    phyloseq_combine_samples - add groupping variable

--- R/phyloseq_combine_samples.R
+++ R/phyloseq_combine_samples.R
@@ -1,15 +1,52 @@
 ## Function to combine samples (sum OTU abundance) into a single sample
-phyloseq_combine_samples <- function(phys){
+phyloseq_combine_samples <- function(physeq, variable = NULL){
 
-  ## Remove sample metadata
-  if(!is.null(phyloseq::sample_data(phys, errorIfNULL=F))){ phys@sam_data <- NULL }
-  
-  ## Estimate OTU total abundance 
-  tss <- phyloseq::taxa_sums(phys)
+  ## Function to combine all samples into a single one
+  comb_phys <- function(phys){
+    ## Remove sample metadata
+    if(!is.null(phyloseq::sample_data(phys, errorIfNULL = FALSE))){ phys@sam_data <- NULL }
+    
+    ## Estimate OTU total abundance 
+    tss <- phyloseq::taxa_sums(phys)
 
-  ## Replace OTU table
-  ott <- matrix(data = tss, ncol = 1, dimnames = list(names(tss), "Total"))
-  phyloseq::otu_table(phys) <- phyloseq::otu_table(ott, taxa_are_rows = TRUE)
-  
-  return(phys)
+    ## Replace OTU table
+    ott <- matrix(data = tss, ncol = 1, dimnames = list(names(tss), "Total"))
+    phyloseq::otu_table(phys) <- phyloseq::otu_table(ott, taxa_are_rows = TRUE)
+    
+    return(phys)
+  }
+
+  ### All samples into a single one (no variables specified)
+  if(is.null(variable)){
+    res <- comb_phys(physeq)
+  } else {
+  ### Combine samples by group
+
+    ## Split samples by group
+    pps <- phyloseq_sep_variable(physeq, variable = variable, drop_zeroes = FALSE)
+
+    ## Combine OTUs by sample group
+    ppc <- plyr::llply(.data = pps, .fun = comb_phys)
+
+    ## Combine phyloseq objects into a single object
+    ## using for-loop and merge_phyloseq  -- very slow for large number of groups
+    # res <- ppc[[1]]
+    # for(i in 2:length(ppc)){
+    #   res <- phyloseq::merge_phyloseq(res, ppc[[i]])
+    # }
+    # rm(i)
+
+    ## Extract OTU tables
+    ppc <- plyr::llply(.data = ppc, .fun = function(x){ as.data.frame(phyloseq::otu_table(x)) })
+
+    ## Merge OTU tables and rename samples
+    ppc <- do.call(cbind, ppc)
+    colnames(ppc) <- names(pps)
+
+    ## Create phyloseq object with merged data
+    res <- phyloseq::phyloseq(
+              phyloseq::otu_table(ppc, taxa_are_rows = TRUE))
+  }
+
+  return(res)
 }
