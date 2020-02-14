28c6ebd5f5fe5807b53c3c325f7ee65634f13274
Vladimir Mikryukov <vmikryukov@gmail.com>
2020-02-14 10:46:44 +0500

    phyloseq_combine_samples - extract and recover data slots

--- R/phyloseq_combine_samples.R
+++ R/phyloseq_combine_samples.R
@@ -22,6 +22,25 @@ phyloseq_combine_samples <- function(physeq, variable = NULL){
   } else {
   ### Combine samples by group
 
+    ## Extract slots from phyloseq object (later we'll return them)
+    ## and remove them to save RAM
+    if(!is.null(phyloseq::tax_table(physeq, errorIfNULL=F))){
+      taxx <- phyloseq::tax_table(physeq)
+      taxpresent <- TRUE
+      physeq@tax_table <- NULL
+    } else {
+      taxpresent <- FALSE   # no tax_table in phyloseq
+    }
+
+    if(!is.null(phyloseq::phy_tree(physeq, errorIfNULL=F))){
+      phyy <- phyloseq::phy_tree(physeq)
+      phypresent <- TRUE
+      physeq@phy_tree <- NULL
+    } else {
+      phypresent <- FALSE   # no phy_tree in phyloseq
+    }
+
+
     ## Split samples by group
     pps <- phyloseq_sep_variable(physeq, variable = variable, drop_zeroes = FALSE)
 
@@ -46,6 +65,10 @@ phyloseq_combine_samples <- function(physeq, variable = NULL){
     ## Create phyloseq object with merged data
     res <- phyloseq::phyloseq(
               phyloseq::otu_table(ppc, taxa_are_rows = TRUE))
+
+    ## Recover phyloseq slots
+    if(taxpresent == TRUE){ res <- phyloseq::merge_phyloseq(res, taxx) }
+    if(phypresent == TRUE){ res <- phyloseq::merge_phyloseq(res, phyy) }
   }
 
   return(res)
