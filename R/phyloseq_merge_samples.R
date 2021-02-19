44317e1e8ebf01072153c85348f2f284e754ba98
Vladimir Mikryukov <vmikryukov@gmail.com>
2021-02-19 18:00:18 +0200

    add phyloseq_merge_samples

---
+++ R/phyloseq_merge_samples.R
@@ -0,0 +1,31 @@
+
+## Merge samples by name
+## NB. sample metadata of the first ID from `samples_to_merge` will be used as a representative for a group
+phyloseq_merge_samples <- function(phys, samples_to_merge, new_id = NULL){
+
+  ## Split phyloseq into samples that should be merged and the rest
+  ps_m <- prune_samples(sample_names(phys) %in% samples_to_merge, phys)
+  ps_r <- prune_samples(!sample_names(phys) %in% samples_to_merge, phys)
+
+  ## Merge chosen samples
+  ps_mrg <- phyloseq_combine_samples(ps_m)
+
+  ## Assign new sample name
+  if(is.null(new_id)){
+    new_id <- paste(samples_to_merge, collapse = "__")
+  }
+  sample_names(ps_mrg) <- new_id
+
+  ## Recover sample metadata
+  if(!is.null(sample_data(phys, errorIfNULL=F))){
+    ps_meta <- prune_samples(sample_names(phys) %in% samples_to_merge[1], phys)
+    sample_names(ps_meta) <- new_id
+    sample_data(ps_mrg) <- sample_data(ps_meta)
+    rm(ps_meta)
+  }
+
+  ## Merge phyloseq objects back
+  res <- merge_phyloseq(ps_mrg, ps_r)
+
+  return(res)
+}
