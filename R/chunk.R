da92ec179569484e46a167e6ccbe6d249fc63a9e
Vladimir Mikryukov <vmikryukov@gmail.com>
2023-03-01 13:30:22 +0200

    add `chunk_table`

--- R/chunk.R
+++ R/chunk.R
@@ -6,3 +6,18 @@ chunk <- function(x, n){
   if(n == 1){ res <- list(); res[[1]] <- x }
   return(res)
 }
+
+## for integer vector
+# n = 3
+# split(x, sort(x%%n))
+
+
+## Split data.table into chunks
+chunk_table <- function(x, n){
+  if(n == 1){ res <- list(); res[[1]] <- x }
+  if(n > 1) {
+    cc <- chunk(1:nrow(x), n = n)
+    res <- llply(.data = cc, .fun = function(r){ x[ r, ] })
+  }
+  return(res)
+}
