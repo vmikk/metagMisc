f08607deeb851b3b594141131796774d8605e76b
Vladimir Mikryukov <vmikryukov@gmail.com>
2023-03-08 17:28:54 +0200

    `read_m8` - add base-package option

--- R/read_m8.R
+++ R/read_m8.R
@@ -1,6 +1,13 @@
 
 ## Function to read BLAST output
 read_m8 <- function(x, blast_colz = NULL, package = "data.table"){
+
+  ## Load data with built-in R commands
+  if(package == "base"){
+    if(grepl(pattern = "\\.gz$", x)){ x <- gzfile(x) }
+    res <- read.delim(file = x, header = FALSE, sep = "\t", stringsAsFactors = FALSE)
+  }
+
   ## Load data with `data.table` package
   if(package == "data.table"){
     res <- fread(file = x, header = FALSE, sep = "\t")
