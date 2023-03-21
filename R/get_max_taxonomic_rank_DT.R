5ffe383acc769ce9d1b378b6490f9d9ed4ba004e
Vladimir Mikryukov <vmikryukov@gmail.com>
2023-03-21 14:03:46 +0200

    add `get_max_taxonomic_rank_DT` function

---
+++ R/get_max_taxonomic_rank_DT.R
@@ -0,0 +1,70 @@
+get_max_taxonomic_rank_DT <- function(x, return_rank_only = FALSE){
+
+  ## Input data
+  inp_class <- class(x)
+
+  ## If input is of class 'phyloseq'
+  if("phyloseq" %in% inp_class || "taxonomyTable" %in% inp_class){
+
+    if(is.null(phyloseq::tax_table(x, errorIfNULL = F))){
+      stop("Error: taxonomy table slot is empty in the input data.\n")
+    }
+
+    ## Extract taxonomy table
+    otu_names <- phyloseq::taxa_names(x)
+    x <- as.data.frame(phyloseq::tax_table(x), stringsAsFactors = F)
+  }
+
+  ## Convert to data.table object (rownames would be lost!)
+  otu_names <- rownames(x)
+  setDT(x)
+
+  ## Tax rank names
+  tax_cols <- copy(colnames(x))
+  # tax_cols <-  c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species")
+
+  ## If there are factors in the input data frame this can cause an "Error: cannot allocate vector of size ... Gb"
+  ## Convert all factors to character
+  if("factor" %in% plyr::laply(.data = x, .fun = class)){
+    x <- x[, lapply(.SD, as.character), .SDcols = tax_cols]
+  }
+
+  ## Function to extract lowest taxonomy rank observed
+  get_max_rank <- function(z){ 
+    rnk <- which(!is.na(z))
+    if(length(rnk) > 0){
+      return( tax_cols[ max(rnk) ]  )
+    } else {
+      return(NA)
+    }
+  }
+
+  ## Function to extract lowest taxon
+  get_max_taxon <- function(z){
+    z <- z[ !is.na(z) ]
+    if(length(z)==0){ 
+      return(NA) 
+    } else { 
+      return(z[length(z)]) 
+    }
+  }
+
+  ## Find the lowest taxa for each row
+  x[, `:=` (
+      LowestTaxon = apply(.SD, 1, get_max_taxon),
+      RankName = apply(.SD, 1, get_max_rank) ),
+    .SDcols = tax_cols
+    ]
+
+  x$RankName <- factor(x = x$RankName, levels = tax_cols)
+
+  ## Return only ranks
+  if(return_rank_only == TRUE){
+    res <- res$RankName
+  } else {
+  ## Or full table
+    res <- data.frame(TaxaName = otu_names, x, stringsAsFactors = F)
+  }
+
+  return(res)
+}
