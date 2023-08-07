e30d03a405e9b97478d6b14b3c89f345cf3371f7
Vladimir Mikryukov <vmikryukov@gmail.com>
2023-08-07 11:09:46 +0300

    roxygenize `chunk` function

--- R/chunk.R
+++ R/chunk.R
@@ -1,6 +1,15 @@
 
-# Split a vector into chunks equal size
-# x = vector; n = number of chunks
+#' Split a vector into chunks equal size
+#'
+#' @param x A vector (e.g., character or numeric vector)
+#' @param n The number of chunks required (integer)
+#'
+#' @return List (of the length defined by the number of chunks) with vectors
+#' @export
+#'
+#' @examples
+#' chunk(x = letters, n = 4)
+#'
 chunk <- function(x, n){
   if(n > 1) { res <- split(x, cut(seq_along(x), n, labels = FALSE)) }
   if(n == 1){ res <- list(); res[[1]] <- x }
