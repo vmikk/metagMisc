0110c337e0fd37401b5ab297a18b9f07bd48f1d5
Vladimir Mikryukov <vmikryukov@gmail.com>
2023-12-05 16:28:42 +0200

    roxygenize `chunk_table`

--- R/chunk.R
+++ R/chunk.R
@@ -21,7 +21,17 @@ chunk <- function(x, n){
 # split(x, sort(x%%n))
 
 
-## Split data.table into chunks
+#' Split data.table into chunks
+#'
+#' @param x A data.table
+#' @param n The number of chunks required (integer)
+#'
+#' @return List (of the length defined by the number of chunks) with parts of the data.table
+#' @export
+#'
+#' @examples
+#' chunk_table(x = data.table(Letter = letters[1:12]), n = 4)
+#'
 chunk_table <- function(x, n){
   if(n == 1){ res <- list(); res[[1]] <- x }
   if(n > 1) {
