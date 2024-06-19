c7379122ad8a7d74140b4ff8de1a96fb08453dad
Vladimir Mikryukov <vmikryukov@gmail.com>
2024-06-19 20:09:52 +0300

    `leading_zero` - upd docs

--- R/leading_zero.R
+++ R/leading_zero.R
@@ -5,13 +5,20 @@
 #' @param z Number of significant digits of the resulting number with leading zeros (if NULL, it will be detected automatically)
 #' @param mode "add" or "remove" leading zero
 #'
-#' @return vector with (mode = "add) or without (mode = "remove") leading zeros
+#' @details 
+#' In 'add' mode, the function adds leading zeros to the numbers to make them all the same length.  
+#' In 'remove' mode, the function removes leading zeros. 
+#' The pattern used for removing leading zeros removes all zeros that are either at the beginning of the string
+#' (e.g., '0001' -> '1') or following non-digit characters (e.g., 'sample001' -> 'sample1').
+#'
+#' @return A vector with (mode = "add) or without (mode = "remove") leading zeros
 #' @export
 #'
 #' @examples
 #' leading_zero(1:10, z=3)
 #' leading_zero(c(1,10,100,1000,10000))
 #' leading_zero(c("001", "1001", "g01", "0abc"), mode = "remove")
+#' leading_zero(x = "sample001", mode = "remove")  # sample1
 #'
 leading_zero <- function(x, z = NULL, mode = "add"){
 
