bbdc10788e04470a6cf61285ec6e94d318e5bc8b
Vladimir Mikryukov <vmikryukov@gmail.com>
2025-08-31 17:47:07 +0300

    add `phyloseq_sep_samp_chunks`

---
+++ R/phyloseq_sep_samp_chunks.R
@@ -0,0 +1,37 @@
+phyloseq_sep_samp_chunks <- function(physeq, nchunks = NULL, drop_zeroes = T){
+    
+  ## Check if nchunks is specified
+  if(is.null(nchunks)){
+    stop("Error: number of samples is not specified.\n")
+  }
+
+  ## If there is just a single sample in phyloseq
+  if(phyloseq::nsamples(physeq) == 1){
+    cat("Warning: there is only one sample in the resulting list.\n")
+    res <- list()
+    res[[1]] <- physeq
+  } else {
+  ## If there are multiple samples
+
+  ## Extract sample names
+  smp <- phyloseq::sample_names(physeq)
+
+  ## Split samples into N groups
+  chunks <- chunk(x = smp, n = nchunks)
+  # chunks <- chunk(x = 1:nsamples(physeq), n = nchunks)
+
+  ## Extract samples
+  res <- plyr::llply(.data = chunks, .fun = function(samples){ 
+    phyloseq::prune_samples(sample_names(physeq) %in% samples, physeq)
+    })
+
+  }
+
+  ## Remove taxa with zero abundance
+  if(drop_zeroes == TRUE){
+    res <- plyr::llply(.data = res, .fun = function(x){ phyloseq::prune_taxa(phyloseq::taxa_sums(x) > 0, x) })
+  }
+
+  return(res)
+}
+
