a2ebd4f58a3506e4a6aa873ac266db1838a4a795
Vladimir Mikryukov <v.mikryukov@gmail.com>
2025-11-09 17:15:50 +0200

    upd tests for phyloseq_otu_occurrence

--- inst/tinytest/test_phyloseq_otu_occurrence.R
+++ inst/tinytest/test_phyloseq_otu_occurrence.R
@@ -11,3 +11,13 @@ if ("SampleType" %in% colnames(sample_data(ps))) {
   expect_true(ntaxa(gp_count) == ntaxa(ps))
   expect_true(all(unique(sample_data(ps)$SampleType) %in% sample_names(gp_count)))
 }
+
+## Return data.frame in wide format (relative frequency)
+if ("SampleType" %in% colnames(sample_data(ps))) {
+  gp_wide <- phyloseq_otu_occurrence(ps, variable = "SampleType", taxa_frequency = "percentage", justdf = TRUE, long = FALSE)
+  expect_true(is.data.frame(gp_wide))
+  expect_true("Taxa" %in% colnames(gp_wide))
+  expect_true(all(unique(sample_data(ps)$SampleType) %in% colnames(gp_wide)))
+  expect_true(all(taxa_names(ps) %in% gp_wide$Taxa))
+}
+
