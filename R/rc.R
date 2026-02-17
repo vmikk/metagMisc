27e678b0464b264a69da47cb6d1a05df98e4b6ab
Vladimir Mikryukov <v.mikryukov@gmail.com>
2026-02-17 19:48:28 +0200

    roxygenize `rc`

--- R/rc.R
+++ R/rc.R
@@ -1,6 +1,22 @@
 
-
-## Reverse-complement DNA sequence (supporting IUPAC degeneracies)
+#' Compute reverse complement of DNA/RNA sequences
+#'
+#' @description
+#' Computes the reverse complement for each sequence in a character vector,
+#' supporting standard nucleotides and common IUPAC ambiguity codes.
+#' Useful for strand-aware sequence processing in amplicon and marker-gene workflows.
+#'
+#' @param x Character vector of nucleotide sequences to reverse-complement.
+#'   Supports upper- and lower-case symbols, including ambiguous IUPAC bases.
+#'
+#' @details
+#' Notes: Non-IUPAC characters are preserved as-is.
+#'
+#' @return Character vector of the same length as \code{x}, containing
+#'   reverse-complemented sequences.
+#'
+#' @importFrom stringi stri_reverse
+#' @export
 rc <- function(x){
   # x = character vector
 
