40d04b298492a1c165011ac173a3ad470095b8df
Vladimir Mikryukov <v.mikryukov@gmail.com>
2026-04-11 09:55:01 +0300

    upd `zero_pad`

--- R/zero_pad.R
+++ R/zero_pad.R
@@ -19,7 +19,7 @@
 #' \itemize{
 #'   \item Zero values are handled correctly
 #'   \item Negative numbers are supported (the minus sign does not count toward digit padding)
-#'   \item Non-finite values (Inf, -Inf, NaN) are treated as having 0 digits for calculation purposes
+#'   \item Non-finite values (Inf, -Inf, NaN) are converted to character strings
 #' }
 #'
 #' @return Character vector of the same length as \code{x}, with numbers formatted 
@@ -46,10 +46,53 @@ zero_pad <- function(x, ndigits = NULL){
 
   ## Automatically choose the number of digits
   if(is.null(ndigits)){
-    res <- sprintf(paste("%0", n_int_digits(max(x)) + 1, "d", sep = ""), x)
-  } else {
-  ## Use user-defined number
-    res <- sprintf(paste("%0", ndigits, "d", sep = ""), x)
+    ## Only consider finite values for calculating ndigits
+    finite_vals <- x[is.finite(x) & !is.na(x)]
+    if(length(finite_vals) == 0) {
+      ## If no finite values, default to 1 digit
+      ndigits <- 1
+    } else {
+      ndigits <- max(n_int_digits(finite_vals)) + 1
+    }
   }
+  
+  ## Initialize result vector
+  res <- character(length(x))
+  
+  ## Create masks for different value types
+  finite_mask <- is.finite(x) & !is.na(x)
+  na_mask <- is.na(x) & !is.nan(x)
+  nan_mask <- is.nan(x)
+  pos_inf_mask <- !is.na(x) & is.infinite(x) & x > 0
+  neg_inf_mask <- !is.na(x) & is.infinite(x) & x < 0
+  
+  ## Format finite values (including negative numbers and zero)
+  if(any(finite_mask)) {
+    finite_vals <- x[finite_mask]
+    
+    ## For negative numbers, we need an extra digit to account for the minus sign
+    ## since sprintf counts the minus sign in the total width
+    positive_mask <- finite_vals >= 0
+    negative_mask <- finite_vals < 0
+    
+    if(any(positive_mask)) {
+      fmt_pos <- paste0("%0", ndigits, "d")
+      res[finite_mask][positive_mask] <- sprintf(fmt_pos, finite_vals[positive_mask])
+    }
+    
+    if(any(negative_mask)) {
+      fmt_neg <- paste0("%0", ndigits + 1, "d")
+      res[finite_mask][negative_mask] <- sprintf(fmt_neg, finite_vals[negative_mask])
+    }
+  }
+  
+  ## Handle non-finite values as character strings
+  if(non_finits_found == TRUE){
+    res[na_mask] <- "NA"
+    res[nan_mask] <- "NaN"
+    res[pos_inf_mask] <- "Inf"
+    res[neg_inf_mask] <- "-Inf"
+  }
+  
   return(res)
 }
